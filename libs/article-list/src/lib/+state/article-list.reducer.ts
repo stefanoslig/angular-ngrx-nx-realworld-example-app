04256ff8ab6722e12a6a19b6756746616af8f9d3
stefanoslig <stefanoslignos@gmail.com>
2022-01-15 09:10:37 +0100

    Add the createFeature helper (article-list lib)

--- libs/article-list/src/lib/+state/article-list.reducer.ts
+++ libs/article-list/src/lib/+state/article-list.reducer.ts
@@ -1,16 +1,12 @@
 import { Article } from '@angular-ngrx-nx-realworld-example-app/api';
-import { Action, createReducer, on } from '@ngrx/store';
+import { Action, createFeature, createReducer, on } from '@ngrx/store';
 import * as ArticleListActions from './article-list.actions';
 
 export const articleListFeatureKey = 'articles-list';
 
-export interface ArticleList {
+export interface ArticleListState {
   listConfig: ArticleListConfig;
   articles: Articles;
-}
-
-export interface ArticleListState {
-  readonly [articleListFeatureKey]: ArticleList;
 }
 
 export interface ArticleListConfig {
@@ -36,7 +32,7 @@ export interface Articles {
   loading: boolean;
 }
 
-export const articleListInitialState: ArticleList = {
+export const articleListInitialState: ArticleListState = {
   listConfig: {
     type: 'ALL',
     currentPage: 1,
@@ -52,64 +48,49 @@ export const articleListInitialState: ArticleList = {
   },
 };
 
-const reducer = createReducer(
-  articleListInitialState,
-  on(ArticleListActions.setListPage, (state, action) => {
-    const filters = {
-      ...state.listConfig.filters,
-      offset: state.listConfig.filters.limit * (action.page - 1),
-    };
-    const listConfig = {
-      ...state.listConfig,
-      currentPage: action.page,
-      filters,
-    };
-    return { ...state, listConfig };
-  }),
-  on(ArticleListActions.setListConfig, (state, action) => ({
-    ...state,
-    listConfig: action.config,
-  })),
-  on(ArticleListActions.loadArticles, (state, _) => {
-    const articles = { ...state.articles, loading: true };
-    return { ...state, articles };
-  }),
-  on(ArticleListActions.loadArticlesSuccess, (state, action) => {
-    const articles = {
-      ...state.articles,
-      entities: action.articles,
-      articlesCount: action.articlesCount,
-      loading: false,
-      loaded: true,
-    };
-    return { ...state, articles };
-  }),
-  on(ArticleListActions.loadArticlesFail, (state, _) => {
-    const articles = {
-      ...state.articles,
-      entities: [],
-      articlesCount: 0,
-      loading: false,
-      loaded: true,
-    };
-    return { ...state, articles };
-  }),
-  on(ArticleListActions.unFavoriteSuccess, ArticleListActions.favoriteSuccess, (state, action) => ({
-    ...state,
-    articles: replaceArticle(state.articles, action.article),
-  })),
-);
+export const articleListFeature = createFeature({
+  name: 'articlesList',
+  reducer: createReducer(
+    articleListInitialState,
+    on(ArticleListActions.setListPage, (state, { page }) => {
+      const filters = {
+        ...state.listConfig.filters,
+        offset: state.listConfig.filters.limit * (page - 1),
+      };
+      const listConfig = {
+        ...state.listConfig,
+        currentPage: page,
+        filters,
+      };
+      return { ...state, listConfig };
+    }),
+    on(ArticleListActions.setListConfig, (state, { config }) => ({
+      ...state,
+      listConfig: config,
+    })),
+    on(ArticleListActions.loadArticles, (state) => {
+      const articles = { ...state.articles, loading: true };
+      return { ...state, articles };
+    }),
+    on(ArticleListActions.loadArticlesSuccess, (state, { articles, articlesCount }) => {
+      return { ...state, entities: articles, articlesCount, loading: false, loaded: true };
+    }),
+    on(ArticleListActions.loadArticlesFail, (state) => {
+      return { ...state, entities: [], articlesCount: 0, loading: false, loaded: true };
+    }),
+    on(ArticleListActions.unFavoriteSuccess, ArticleListActions.favoriteSuccess, (state, { article }) => ({
+      ...state,
+      articles: replaceArticle(state.articles, article),
+    })),
+  ),
+});
 
 function replaceArticle(articles: Articles, payload: Article): Articles {
-  const articleIndex = articles.entities.findIndex(a => a.slug === payload.slug);
+  const articleIndex = articles.entities.findIndex((a) => a.slug === payload.slug);
   const entities = [
     ...articles.entities.slice(0, articleIndex),
     Object.assign({}, articles.entities[articleIndex], payload),
     ...articles.entities.slice(articleIndex + 1),
   ];
   return { ...articles, entities, loading: false, loaded: true };
-}
-
-export function articleListReducer(state: ArticleList | undefined, action: Action): ArticleList {
-  return reducer(state, action);
 }
