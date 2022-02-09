46d29ed35ed4232fc166121291cbb488bc5334f2
stefanoslig <stefanoslignos@gmail.com>
2022-02-09 14:43:30 +0100

    Add eslint-plugin-ngrx

--- apps/conduit/src/app/app.component.spec.ts
+++ apps/conduit/src/app/app.component.spec.ts
@@ -2,8 +2,8 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
 import { RouterTestingModule } from '@angular/router/testing';
-import { FooterComponent } from '@default/layout/footer/footer.component';
-import { NavbarComponent } from '@default/layout/navbar/navbar.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
 import { StoreModule } from '@ngrx/store';
 import { AuthFacade, LocalStorageJwtService } from '@angular-ngrx-nx-realworld-example-app/auth';
 
