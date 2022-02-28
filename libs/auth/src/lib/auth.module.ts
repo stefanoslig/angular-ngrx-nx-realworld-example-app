d28436cb29281ef051a991d83400c2d6eb8132ab
stefanoslig <stefanoslignos@gmail.com>
2022-02-28 09:42:28 +0100

    Remove unused imports

--- libs/auth/src/lib/auth.module.ts
+++ libs/auth/src/lib/auth.module.ts
@@ -1,19 +1,10 @@
-import {
-  DynamicFormComponentModule,
-  ListErrorsComponentModule,
-} from '@angular-ngrx-nx-realworld-example-app/ngrx-forms';
-import { CommonModule } from '@angular/common';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
-
 import { AuthEffects } from './+state/auth.effects';
 import { authFeature } from './+state/auth.reducer';
-import { AuthGuardService } from './auth-guard.service';
-import { AuthService } from './auth.service';
-import { LocalStorageJwtService } from './local-storage-jwt.service';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { TokenInterceptorService } from './token-interceptor.service';
@@ -30,11 +21,7 @@ const authRouting = RouterModule.forChild([
 ]);
 
 @NgModule({
-  imports: [
-    authRouting,
-    StoreModule.forFeature(authFeature),
-    EffectsModule.forFeature([AuthEffects]),
-  ],
+  imports: [authRouting, StoreModule.forFeature(authFeature), EffectsModule.forFeature([AuthEffects])],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
