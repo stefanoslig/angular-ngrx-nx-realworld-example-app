58fbd2016fef63241d31081388318dbe092f5329
George Spanos <george.sp@trgint.com>
2022-06-02 16:01:45 +0300

    Inject api_url as a provider in app.component to remove circular dependency

--- libs/core/http-client/src/index.ts
+++ libs/core/http-client/src/index.ts
@@ -1,1 +1,2 @@
 export * from './lib/api.service';
+export * from './lib/api-url.token';
---
+++ libs/core/http-client/src/lib/api-url.token.ts
@@ -0,0 +1,3 @@
+import { InjectionToken } from '@angular/core';
+
+export const API_URL = new InjectionToken<string>('API_URL');
