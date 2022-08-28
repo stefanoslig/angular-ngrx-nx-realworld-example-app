3b15886742664290a627a84ec8d0547d53652138
stefanoslig <stefanoslignos@gmail.com>
2022-08-28 19:42:17 +0200

    Use the new provideRouter method instead of the RouterModule.forRoot

--- apps/conduit/src/main.ts
+++ apps/conduit/src/main.ts
@@ -1,9 +1,8 @@
 import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication, BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
+import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
-import { EffectsModule } from '@ngrx/effects';
 import { StoreRouterConnectingModule } from '@ngrx/router-store';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
@@ -16,6 +15,7 @@ import {
 } from '@realworld/core/error-handler';
 import { NgrxFormsEffects, ngrxFormsFeature } from '@realworld/core/forms';
 import { API_URL } from '@realworld/core/http-client';
+import { EffectsModule } from '@ngrx/effects';
 
 if (environment.production) {
   enableProdMode();
@@ -36,60 +36,52 @@ const rootInterceptors = [
 
 bootstrapApplication(AppComponent, {
   providers: [
+    provideRouter([
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+      },
+      {
+        path: 'home',
+        loadChildren: () => import('@realworld/home/src/lib/home.routes').then((home) => home.HOME_ROUTES),
+      },
+      {
+        path: 'login',
+        loadComponent: () => import('@realworld/auth/feature-auth').then((m) => m.LoginComponent),
+      },
+      {
+        path: 'register',
+        loadComponent: () => import('@realworld/auth/feature-auth').then((m) => m.RegisterComponent),
+      },
+      {
+        path: 'article',
+        loadChildren: () => import('@realworld/articles/article').then((m) => m.ARTICLE_ROUTES),
+      },
+      {
+        path: 'settings',
+        loadComponent: () =>
+          import('@realworld/settings/feature-settings').then((settings) => settings.SettingsComponent),
+      },
+      {
+        path: 'editor',
+        loadChildren: () => import('@realworld/articles/article-edit').then((article) => article.ARTICLE_EDIT_ROUTES),
+        canActivate: [AuthGuardService],
+      },
+      {
+        path: 'profile',
+        loadChildren: () => import('@realworld/profile/feature-profile').then((profile) => profile.PROFILE_ROUTES),
+      },
+    ]),
     importProvidersFrom(
       BrowserModule,
       HttpClientModule,
-      RouterModule.forRoot(
-        [
-          {
-            path: '',
-            redirectTo: 'home',
-            pathMatch: 'full',
-          },
-          {
-            path: 'home',
-            loadChildren: () => import('@realworld/home/src/lib/home.routes').then((home) => home.HOME_ROUTES),
-          },
-          {
-            path: 'login',
-            loadComponent: () => import('@realworld/auth/feature-auth').then((m) => m.LoginComponent),
-          },
-          {
-            path: 'register',
-            loadComponent: () => import('@realworld/auth/feature-auth').then((m) => m.RegisterComponent),
-          },
-          {
-            path: 'article',
-            loadChildren: () => import('@realworld/articles/article').then((m) => m.ARTICLE_ROUTES),
-          },
-          {
-            path: 'settings',
-            loadComponent: () =>
-              import('@realworld/settings/feature-settings').then((settings) => settings.SettingsComponent),
-          },
-          {
-            path: 'editor',
-            loadChildren: () =>
-              import('@realworld/articles/article-edit').then((article) => article.ARTICLE_EDIT_ROUTES),
-            canActivate: [AuthGuardService],
-          },
-          {
-            path: 'profile',
-            loadChildren: () => import('@realworld/profile/feature-profile').then((profile) => profile.PROFILE_ROUTES),
-          },
-        ],
-        {
-          initialNavigation: 'enabledBlocking',
-          useHash: true,
-          relativeLinkResolution: 'legacy',
-        },
-      ),
+      EffectsModule.forRoot([ErrorHandlerEffects, AuthEffects, NgrxFormsEffects]),
       StoreModule.forRoot({
         auth: authFeature.reducer,
         errorHandler: errorHandlerFeature.reducer,
         ngrxForms: ngrxFormsFeature.reducer,
       }),
-      EffectsModule.forRoot([ErrorHandlerEffects, AuthEffects, NgrxFormsEffects]),
       !environment.production ? StoreDevtoolsModule.instrument() : [],
       StoreRouterConnectingModule.forRoot(),
     ),
