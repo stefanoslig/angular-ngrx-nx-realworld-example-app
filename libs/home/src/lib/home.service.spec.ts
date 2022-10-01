c5539fc1391c792e5a1c288c72abef264ed40f88
stefanoslig <stefanoslignos@gmail.com>
2022-10-01 20:56:20 +0200

    fix unit tests - home lib

--- libs/home/src/lib/home.service.spec.ts
+++ libs/home/src/lib/home.service.spec.ts
@@ -1,14 +1,14 @@
-import { ApiService } from '@realworld/core/api-types';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { inject, TestBed } from '@angular/core/testing';
-
+import { ApiService } from '@realworld/core/http-client';
+import { MockProvider } from 'ng-mocks';
 import { HomeService } from './home.service';
 
 describe('HomeService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      providers: [HomeService, ApiService],
+      providers: [HomeService, MockProvider(ApiService)],
     });
   });
 
