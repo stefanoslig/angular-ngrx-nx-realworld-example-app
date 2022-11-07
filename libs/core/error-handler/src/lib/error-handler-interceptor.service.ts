b09dcdd1723da1b4274a2248ee1c4be5b2186461
stefanoslig <stefanoslignos@gmail.com>
2022-11-07 21:34:56 +0100

    refactor: Remove error-handler facade

--- libs/core/error-handler/src/lib/error-handler-interceptor.service.ts
+++ libs/core/error-handler/src/lib/error-handler-interceptor.service.ts
@@ -1,25 +1,25 @@
 import { HttpErrorResponse, HttpEvent, HttpHandlerFn, HttpRequest } from '@angular/common/http';
 import { inject } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-
-import { ErrorHandlerFacade } from './+state/error-handler.facade';
+import { throw401Error, throw404Error } from './+state/error-handler.actions';
 
 export const errorHandlingInterceptor = (
   request: HttpRequest<any>,
   next: HttpHandlerFn,
 ): Observable<HttpEvent<any>> => {
-  const errorHandlerFacade = inject(ErrorHandlerFacade);
+  const store = inject(Store);
 
   return next(request).pipe(
     catchError((error) => {
       if (error instanceof HttpErrorResponse) {
         switch (error.status) {
           case 401:
-            errorHandlerFacade.throw401Error(error);
+            store.dispatch(throw401Error({ error }));
             break;
           case 404:
-            errorHandlerFacade.throw404Error(error);
+            store.dispatch(throw404Error({ error }));
             break;
           default:
             throwError(error);
