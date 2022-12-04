eb3d6746623d9c91849a7b504957f2c390e81116
stefanoslig <stefanoslignos@gmail.com>
2022-12-04 22:17:06 +0100

    refactor: Remove forms facade

--- libs/core/forms/src/index.ts
+++ libs/core/forms/src/index.ts
@@ -3,5 +3,5 @@ export { DynamicFormComponent } from './lib/dynamic-form/dynamic-form.component'
 export * from './lib/+state/forms.actions';
 export * from './lib/+state/forms.reducer';
 export * from './lib/+state/forms.effects';
-export * from './lib/+state/forms.facade';
+export * from './lib/+state/forms.selectors';
 export * from './lib/+state/forms.interfaces';
