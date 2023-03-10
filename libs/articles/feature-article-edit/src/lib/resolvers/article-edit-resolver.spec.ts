7b1d3b20b381cbf9171abf14baa8b48b100dc688
stefanoslig <stefanoslignos@gmail.com>
2023-03-10 20:40:19 +0100

    refactor: Migrate to functional resolvers/guards - feature-article-edit

--- libs/articles/feature-article-edit/src/lib/resolvers/article-edit-resolver.spec.ts
+++ libs/articles/feature-article-edit/src/lib/resolvers/article-edit-resolver.spec.ts
@@ -1,4 +1,3 @@
-import { EnvironmentInjector } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { MockStore, provideMockStore } from '@ngrx/store/testing';
 import { articleEditResolver } from './article-edit-resolver';
@@ -6,7 +5,7 @@ import { cold } from 'jasmine-marbles';
 import { articleActions } from '@realworld/articles/data-access';
 import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
-const mockRoute = { params: { slug: '1' } } as unknown as ActivatedRouteSnapshot;
+const mockRoute: ActivatedRouteSnapshot = { params: { slug: '1' } } as unknown as ActivatedRouteSnapshot;
 
 describe('articleEditResolver', () => {
   let store: MockStore;
@@ -22,10 +21,17 @@ describe('articleEditResolver', () => {
   it('should return `true` and dispatch articleActions.loadArticle action when slug is not undefined', () => {
     const dispatchSpy = jest.spyOn(store, 'dispatch');
 
-    const result = TestBed.inject(EnvironmentInjector).runInContext(
-      articleEditResolver(mockRoute, {} as RouterStateSnapshot) as any,
+    const result = TestBed.runInInjectionContext(
+      () => articleEditResolver(mockRoute, {} as RouterStateSnapshot) as any,
     );
     expect(dispatchSpy).toHaveBeenCalledWith(articleActions.loadArticle({ slug: '1' }));
+    expect(result).toBeObservable(cold('(a|)', { a: true }));
+  });
+
+  it('should just return `true` when slug is undefined', () => {
+    const result = TestBed.runInInjectionContext(
+      () => articleEditResolver(mockRoute, {} as RouterStateSnapshot) as any,
+    );
     expect(result).toBeObservable(cold('(a|)', { a: true }));
   });
 });
