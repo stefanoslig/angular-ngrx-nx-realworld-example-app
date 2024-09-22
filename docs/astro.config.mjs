95001f0eba386b5c340f3b04930fc29ecc629778
stefanoslig <stefanoslignos@gmail.com>
2024-09-22 22:14:34 +0200

    new logo for the docs page

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -6,22 +6,20 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
   integrations: [
     starlight({
-      title: 'My Docs',
+      logo: {src: './src/assets/hello.png', replacesTitle: true},
+      title: 'Angular Realworld App',
       social: {
-        github: 'https://github.com/withastro/starlight',
+        github: 'https://github.com/stefanoslig/angular-ngrx-nx-realworld-example-app',
       },
       sidebar: [
         {
-          label: 'Guides',
+          label: 'Architecture',
           items: [
             // Each item here is one entry in the navigation menu.
             { label: 'Example Guide', slug: 'guides/example' },
           ],
         },
-        {
-          label: 'Reference',
-          autogenerate: { directory: 'reference' },
-        },
+        
       ],
     }),
   ],
