00e300fa6f2139faeda28c905cfa10266364ccbe
stefanoslig <stefanoslignos@gmail.com>
2024-09-23 21:57:45 +0200

    documentation

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -6,17 +6,22 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
   integrations: [
     starlight({
-      logo: {src: './src/assets/hello.png', replacesTitle: true},
+      logo: {src: './src/assets/logo.png', replacesTitle: true},
       title: 'Angular Realworld App',
       social: {
         github: 'https://github.com/stefanoslig/angular-ngrx-nx-realworld-example-app',
       },
       sidebar: [
         {
-          label: 'Architecture',
+          label: 'Angular Realworld App',
           items: [
             // Each item here is one entry in the navigation menu.
-            { label: 'Example Guide', slug: 'guides/example' },
+            { label: 'Introduction', slug: 'introduction' },
+            { label: 'App explanation', slug: 'explanation' },
+            { label: 'Angular features', slug: 'features' },
+            { label: 'Architecture', slug: 'architecture' },
+            { label: 'How to contribute', slug: 'contributions' },
+            { label: 'How to run the app locally', slug: 'local-dev' },
           ],
         },
         
