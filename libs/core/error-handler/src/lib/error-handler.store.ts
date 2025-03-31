3765d44becfd53236348d6c3ce1990096c079fd4
stefanoslig <stefanoslignos@gmail.com>
2025-03-31 15:42:37 +0200

    fix: should not redirect user from the registr page

--- libs/core/error-handler/src/lib/error-handler.store.ts
+++ libs/core/error-handler/src/lib/error-handler.store.ts
@@ -1,19 +1,21 @@
 import { signalStore, withState, withMethods, patchState } from '@ngrx/signals';
 import { inject } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ErrorHandlerState, errorHandlerInitialState } from './models/error-handler.state';
 import { HttpErrorResponse } from '@angular/common/http';
 
 export const ErrorHandlerStore = signalStore(
   { providedIn: 'root' },
   withState<ErrorHandlerState>(errorHandlerInitialState),
-  withMethods((store, router = inject(Router)) => ({
+  withMethods((store, router = inject(Router), route = inject(ActivatedRoute)) => ({
     handleError401: (error: HttpErrorResponse) => {
       patchState(store, {
         code: error.status,
         message: error.message,
       });
-      router.navigate(['/login']);
+      if (route.snapshot.children[0].url[0].path !== 'register') {
+        router.navigate(['/login']);
+      }
     },
     handleError404: (error: HttpErrorResponse) => {
       patchState(store, {
